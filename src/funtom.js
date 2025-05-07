495c7605f04d0bb25c015de00c56b261eaf57edb
midoliy <midoliy@outlook.jp>
2025-05-07 09:14:17 +0900

    wip

--- src/funtom.js
+++ src/funtom.js
@@ -2,16 +2,8 @@ Object.prototype.pipe = (fx, ...args) => fx(this, ...args);
 Function.prototype.compose = (fx) => (...args) => fx(this(...args));
 Function.prototype.curry = function(...args) {
   const f = this;
-  const curried = function(...args) {
-    if (args.length >= f.length) {
-      return f.apply(f, args);
-    } else {
-      return function (...nextArgs) {
-        return curried.apply(f, args.concat(nextArgs));
-      };
-    }
-  };
-  return args.length >= f.length 
+  const curried = (...args) => args.length >= f.length
     ? f.apply(f, args)
-    : curried.apply(f, args);
+    : (...nextArgs) => curried.apply(f, args.concat(nextArgs));
+  return curried.apply(f, args);
 };
